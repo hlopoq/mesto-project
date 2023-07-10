cad2f1c7b4c8abc257591d9a82588df291c38389
hlopoq <hlopoq@yandex.ru>
2023-07-10 23:04:56 +0300

    fixes in Card, Section

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default class Card {
   ) {
     this._nameValue = data.name;
     this._linkValue = data.link;
-    this._cardId = data.id;
+    this._cardId = data._id;
     this._cardLikes = data.likes;
     this._cardOwner = data.owner;
     this._handleLikeClick = handleLikeClick;
@@ -26,7 +26,7 @@ export default class Card {
 
   _checkCurrentUserLike() {
     const myLike = this._cardLikes.some((item) => {
-      return item.id === this._ownerId;
+      return item._id === this._ownerId;
     });
     if (myLike) {
       this._likeButton.classList.add("card__like-button_active");
@@ -53,10 +53,12 @@ export default class Card {
   toggleLike(updatedLikes) {
     this._cardLikeValue.textContent = updatedLikes.length;
     this._cardLikes = updatedLikes;
-    this._likeButton.classList.toggle(
-      "card__like-active",
-      updatedLikes.some((item) => item.id === this._ownerId)
-    );
+
+    if (!this._likeButton.classList.contains("card__like-button_active")) {
+      this._likeButton.classList.add("card__like-button_active");
+    } else {
+      this._likeButton.classList.remove("card__like-button_active");
+    }
   }
 
   deleteCard() {
@@ -74,7 +76,7 @@ export default class Card {
     this._likeButton = this._item.querySelector(".card__like-button");
     this._deleteButton = this._item.querySelector(".card__trash");
 
-    if (this._cardOwner.id !== this._ownerId) {
+    if (this._cardOwner._id !== this._ownerId) {
       this._deleteButton.classList.add("card__trash_hidden");
     }
     this.toggleLike(this._cardLikes);
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,16 @@
 export default class Section {
-    constructor({ renderer }, containerSelector) {
-        this._container = document.querySelector(containerSelector);
-        this._renderer = renderer;
-    }
+  constructor({ renderer }, containerSelector) {
+    this._container = document.querySelector(containerSelector);
+    this._renderer = renderer;
+  }
 
-    addItem(element) {
-        this._container.prepend(element);
-    }
-    
-    
-    renderItems(items) {
-        items.forEach((item) => {
-        this._renderer(item, this._container);
-        });
-    }
+  addItem(element) {
+    this._container.prepend(element);
+  }
+
+  renderItems(items, id) {
+    items.forEach((item) => {
+      this._renderer(item, id);
+    });
+  }
 }
